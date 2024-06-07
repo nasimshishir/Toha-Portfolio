1182ff39f5a7c4ae76743fb9738635238e39bc9c
nasimshishir <tech17196@gmail.com>
2024-06-07 15:56:01 +0600

    Portfolio added and sentry configured

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Grid from "@/Components/Grid";
+import Hero from "@/Components/Hero";
+import RecentProjects from "@/Components/RecentProjects";
+import { FloatingNav } from "@/Components/ui/FloatingNavbar";
+import { navItems } from "@/data";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+      </div>
+    </main>
+  );
+}
